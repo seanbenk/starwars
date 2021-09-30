95829d22e2b85fd440e08d538e62fb8e4622169e
seanbenk <76204079+seanbenk@users.noreply.github.com>
2021-09-30 18:41:28 +1000

    all content on the film page added and loaded on the front end to minimise server side delay

--- components/character/Character.js
+++ components/character/Character.js
@@ -1,11 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Tooltip } from "@mui/material";
-import CharacterTooltip from "./CharacterTooltip";
+import CharacterTooltip, { tooltipClasses } from "./CharacterTooltip";
 
 function Character({ character }) {
   return (
-    <Tooltip arrow title={<CharacterTooltip character={character} />}>
-      <h3>{character.name}</h3>
+    <Tooltip
+      followCursor
+      arrow
+      title={<CharacterTooltip character={character} />}
+    >
+      <li>{character.name}</li>
     </Tooltip>
   );
 }
